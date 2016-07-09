62e3f708e2253ab6e8be126066b836c6b60b2bd3
yagince <straitwalk@gmail.com>
2016-07-09 23:08:52 +0900

    color

--- src/git_start_main.rs
+++ src/git_start_main.rs
@@ -1,16 +1,20 @@
 extern crate gitx;
 extern crate rustc_serialize;
 extern crate docopt;
-
-use docopt::Docopt;
+extern crate ansi_term;
 
 use std::process::Output;
+
+use docopt::Docopt;
+use ansi_term::Style;
+use ansi_term::Colour::*;
+
 use gitx::git::git::*;
 use gitx::git::branch::*;
 
 const USAGE: &'static str = "
 Usage:
-  git-start <type> <number> <comment>
+  git-start <type> <number> <description>
   git-start (--help | --version)
 
 Options:
@@ -20,10 +24,9 @@ Options:
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    cmd_start: bool,
     arg_type: String,
     arg_number: String,
-    arg_comment: String,
+    arg_description: String,
 }
 
 fn main() {
@@ -31,10 +34,8 @@ fn main() {
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
-    println!("{:?}", args);
-    if args.cmd_start {
-        start(&args);
-    }
+    // println!("{:?}", args);
+    start(&args);
 }
 
 fn start(args: &Args) {
@@ -45,13 +46,18 @@ fn start(args: &Args) {
         "h" => "hotfix",
         t   => t
     };
-    let branch_name = format!("{}/{}-{}", arg_type, args.arg_number, args.arg_comment);
+    let branch_name = format!("{}/{}-{}", arg_type, args.arg_number, args.arg_description);
     let output = git.create_branch(&Branch::new(&branch_name))
         .unwrap_or_else(|e| panic!(e));
+
+    println!("{}:\t{}", Style::new().bold().fg(Green).paint("BranchType"), arg_type);
+    println!("{}:\t{}", Style::new().bold().fg(Green).paint("IssueNumber"), args.arg_number);
+    println!("{}:\t{}", Style::new().bold().fg(Green).paint("Description"), args.arg_description);
+   // TODO: エラーハンドリング
     print_stdout(&output);
 }
 
 fn print_stdout(output : &Output) {
     println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stdout: \n{}", String::from_utf8_lossy(&output.stdout));
 }
