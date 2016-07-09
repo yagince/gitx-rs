dbbfc4aa1075389e2006e262798822a9a0f5880f
yagince <straitwalk@gmail.com>
2016-07-10 01:00:56 +0900

    git cb

--- src/git/branch.rs
+++ src/git/branch.rs
@@ -10,9 +10,20 @@ impl Branches {
     pub fn current_branch(&self) -> Branch {
         self.branches.get(&self.current).unwrap().clone()
     }
+
+    pub fn is_current(&self, branch: &Branch) -> bool {
+        self.current == branch.name
+    }
+
+    pub fn list(&self) -> Vec<Branch> {
+        self.branches
+            .iter()
+            .map(|(_, branch)| branch.clone())
+            .collect()
+    }
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Branch {
     pub name: String,
 }
@@ -39,6 +50,21 @@ mod tests {
             let branches = Branches{ branches: map, current: "second".to_string()};
 
             assert_eq!(branch, branches.current_branch());
+        }
+
+        #[test]
+        fn is_current() {
+            let mut map = HashMap::new();
+            let first = Branch{name: "first".to_string()};
+            map.insert("first".to_string(), first.clone());
+
+            let second = Branch{name: "second".to_string()};
+            map.insert("second".to_string(), second.clone());
+
+            let branches = Branches{ branches: map, current: second.name.clone()};
+
+            assert_eq!(false, branches.is_current(&first));
+            assert_eq!(true,  branches.is_current(&second));
         }
     }
 }
--- src/git_cb_main.rs
+++ src/git_cb_main.rs
@@ -1,21 +1,25 @@
 extern crate gitx;
 extern crate rustc_serialize;
 extern crate docopt;
-extern crate ansi_term;
+extern crate rustbox;
 
-use std::process::Output;
+use std::process::*;
+use std::error::Error;
+use std::default::Default;
 
 use docopt::Docopt;
-use ansi_term::Style;
-use ansi_term::Colour::*;
+use rustbox::{Color, RustBox};
+use rustbox::Key;
 
 use gitx::git::git::*;
 use gitx::git::branch::*;
 
+const VERSION: &'static str = "0.0.1";
+
 const USAGE: &'static str = "
 Usage:
-  git-start <type> <number> <description>
-  git-start (--help | --version)
+  git-cb
+  git-cb (--help | --version)
 
 Options:
   -h --help     Show this screen.
@@ -24,9 +28,7 @@ Options:
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    arg_type: String,
-    arg_number: String,
-    arg_description: String,
+    flag_version: bool,
 }
 
 fn main() {
@@ -34,30 +36,42 @@ fn main() {
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
-    // println!("{:?}", args);
-    start(&args);
+    if args.flag_version {
+        println!("git cb {}", VERSION);
+        exit(0);
+    }
+    exec();
 }
 
-fn start(args: &Args) {
+fn exec() {
+    let rustbox = RustBox::init(Default::default()).unwrap_or_else(|e| panic!(e));
+
     let git = Git::new();
-    let arg_type = match args.arg_type.as_ref() {
-        "f" => "feature",
-        "s" => "spark",
-        "h" => "hotfix",
-        t   => t
-    };
-    let branch_name = format!("{}/{}-{}", arg_type, args.arg_number, args.arg_description);
-    let output = git.create_branch(&Branch::new(&branch_name))
-        .unwrap_or_else(|e| panic!(e));
+    let branches = git.branches();
 
-    println!("{}:\t{}", Style::new().bold().fg(Green).paint("BranchType"), arg_type);
-    println!("{}:\t{}", Style::new().bold().fg(Green).paint("IssueNumber"), args.arg_number);
-    println!("{}:\t{}", Style::new().bold().fg(Green).paint("Description"), args.arg_description);
-   // TODO: エラーハンドリング
-    print_stdout(&output);
-}
+    let mut list = branches.list();
+    list.sort();
 
-fn print_stdout(output : &Output) {
-    println!("status: {}", output.status);
-    println!("stdout: \n{}", String::from_utf8_lossy(&output.stdout));
+    for (i, branch) in list.iter().enumerate() {
+        if branches.is_current(branch) {
+            rustbox.print(1, i, rustbox::RB_BOLD, Color::Green, Color::Default, format!("* {}", branch.name).as_ref());
+        } else {
+            rustbox.print(1, i, rustbox::RB_BOLD, Color::White, Color::Default, branch.name.as_ref());
+        }
+    }
+
+    rustbox.present();
+
+    loop {
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => {
+                match key {
+                    Key::Char('q') => { break; }
+                    _ => { }
+                }
+            },
+            Err(e) => panic!("{}", e.description()),
+            _ => { }
+        }
+    }
 }
